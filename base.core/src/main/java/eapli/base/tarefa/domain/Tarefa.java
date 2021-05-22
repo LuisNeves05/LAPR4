81a76a447d86e38187e7b5c2c10e5a40e555e547
Luís Neves <1191421@isep.ipp.pt>
2021-05-22 01:42:42 +0100

    <LDG-38> Classes base do US

--- base.core/src/main/java/eapli/base/tarefa/domain/Tarefa.java
+++ base.core/src/main/java/eapli/base/tarefa/domain/Tarefa.java
@@ -1,4 +1,16 @@
 package eapli.base.tarefa.domain;
 
+
+import jdk.jfr.Enabled;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Tarefa {
+
+    @Id
+    private Long id;
+
+
 }
--- base.workflow/src/main/java/workflow/base/fluxo/domain/AtividadeAprovacao.java
+++ base.workflow/src/main/java/workflow/base/fluxo/domain/AtividadeAprovacao.java
@@ -1,11 +1,18 @@
 package workflow.base.fluxo.domain;
 
+import eapli.base.tarefa.domain.Tarefa;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Set;
 
 @Entity
 public class AtividadeAprovacao {
 
     @Id
     private Long id;
+
+    @OneToMany
+    private Set<Tarefa> tarefaSet;
 }
