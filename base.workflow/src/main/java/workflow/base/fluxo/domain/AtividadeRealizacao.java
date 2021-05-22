f3f66ee3bc98c63ff90e5a0293966c6d44d7d145
tiagomarante <tiagofmarante@gmail.com>
2021-05-22 01:31:01 +0100

    <LDG-38>  Dependências da tarefa criadas

--- base.workflow/src/main/java/workflow/base/fluxo/domain/AtividadeRealizacao.java
+++ base.workflow/src/main/java/workflow/base/fluxo/domain/AtividadeRealizacao.java
@@ -1,14 +1,19 @@
 package workflow.base.fluxo.domain;
 
+import eapli.base.tarefa.domain.Tarefa;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import java.util.Set;
 
 @Entity
 public class AtividadeRealizacao {
 
     @Id
     private Long id;
-    
+
+
+    private Set<Tarefa> tarefas;
 }
